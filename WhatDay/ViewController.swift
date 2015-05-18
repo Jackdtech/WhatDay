0d916fa4422a3cadece53d49e699b1249a8027d8
Jack Douglas <Jackdtech@hotmail.com>
2015-05-17 18:38:39 -0700

    Initial Commit

---
+++ WhatDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatDay
+//
+//  Created by Jack Douglas on 5/17/15.
+//  Copyright (c) 2015 Jack Douglas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
